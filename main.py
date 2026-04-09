d2f3e15201ea05d389596143a987d5a654474660
Pablo Esteve <estevepablo2005@gmail.com>
2026-04-09 12:18:49 +0200

    test

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import asyncio
 import bot1
+import bot2
+
+RUN_BOT1 = True
+RUN_BOT2 = True
 
 async def main():
-    print("🚀 TEST BOT1 ONLY")
-    await bot1.start_bot()
+    print("🚀 START MAIN")
+
+    tasks = []
+
+    if RUN_BOT1:
+        tasks.append(bot1.start_bot())
+
+    if RUN_BOT2:
+        tasks.append(bot2.start_bot())
+
+    await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
     asyncio.run(main())
